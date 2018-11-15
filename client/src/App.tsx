8ecaeab8e6d2351de2506c321cacba79c9691202
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-15 16:50:23 -0500

    added launch items function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 
-import logo from "./assets/logo.png";
+// import { ApolloProvider } from "react-apollo";
+
+import logo from "./assets/";
 import "./App.css";
 import Launches from "./components/Launches";
 
-const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
-});
+export const App = () => (
+  <div className="App">
+    <img
+      src={logo}
+      alt="SpaceX"
+      style={{
+        width: 300,
+        display: "block",
+        margin: "auto"
+      }}
+    />
+    <Launches />
+  </div>
+);
 
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <img
-            src={logo}
-            alt="SpaceX"
-            style={{
-              width: 300,
-              display: "block",
-              margin: "auto"
-            }}
-          />
-          <Launches />
-        </div>
-      </ApolloProvider>
-    );
-  }
-}
-
-export default App;
+// export class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <img
+//           src={logo}
+//           alt="SpaceX"
+//           style={{
+//             width: 300,
+//             display: "block",
+//             margin: "auto"
+//           }}
+//         />
+//         <Launches />
+//       </div>
+//     );
+//   }
+// }
--- client/src/components/Launches.tsx
+++ client/src/components/Launches.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
-import { Query, OperationVariables } from "react-apollo";
+import { Query, OperationVariables, QueryProps } from "react-apollo";
 import { ApolloError } from "apollo-boost";
+
+import LaunchItem from "./LaunchItem";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -16,23 +18,32 @@ const LAUNCHES_QUERY = gql`
 interface Data {
   loading: boolean;
   error: ApolloError | undefined;
-  data: undefined;
+  data: {} | undefined;
+  key: string[];
 }
 
-class Launches extends Component<{}, Data> {
+interface Props {
+  launch: number;
+}
+
+class Launches extends Component<{}, Data, Props> {
   render() {
     return (
       <div>
-        <h1 className="display-4 my-3">Launches</h1>
-        <Query<{}, OperationVariables> query={LAUNCHES_QUERY}>
+        <h1 className="display-4 my-3">Launches</h1>ReactNode;
+        <Query<OperationVariables, QueryProps> query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
-            if (loading) return;
-            <h4>Loading...</h4>;
+            if (loading) return <h4>Loading...</h4>;
 
             if (error) console.log(error, "error");
-            console.log(data);
 
-            return <h1>test</h1>;
+            return (
+              <div>
+                {data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+              </div>
+            );
           }}
         </Query>
       </div>
